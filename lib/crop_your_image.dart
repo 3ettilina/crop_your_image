1c55b14a6dd81e7297b482b7878e15ad04e1f09d
Bettina Carrizo <betticarrizo@gmail.com>
2024-04-12 16:39:15 -0300

    Fix Uint8List type issues

--- lib/crop_your_image.dart
+++ lib/crop_your_image.dart
@@ -1,8 +1,9 @@
 library crop_your_image;
 
 import 'dart:math';
+import 'dart:typed_data';
 
-import 'package:flutter/foundation.dart';
+import 'package:flutter/foundation.dart' show compute;
 import 'package:flutter/material.dart';
 import 'package:image/image.dart' as image;
 
